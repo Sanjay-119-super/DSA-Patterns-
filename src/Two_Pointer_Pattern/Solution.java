c2035c8285e86aa2d891607f5dd6a5fc306853c7
Sanjay Kumar <coderdevil39@gmail.com>
2026-04-17 08:02:18 +0530

    DAY-2 solved -5 leetcode problems

--- src/Two_Pointer_Pattern/Solution.java
+++ src/Two_Pointer_Pattern/Solution.java
@@ -1,36 +1,38 @@
 package Two_Pointer_Pattern;
 
+import java.util.*;
+
 //DAY-2
 public class Solution {
-    public boolean isPalindrome(String s){
+    public boolean isPalindrome(String s) {
         // Use two Pointer i =0, j=n-1
-        int i=0,
-                j= s.length()-1;
+        int i = 0,
+                j = s.length() - 1;
 
         /*
-        *  "A man, a plan, a canal: Panama"
-        *   i                            j
-        * remove non-alphanumeric chars
-        *
-        *  "amanaplanacanalpanama"
-        *            ij
-        *
-        *
-        * */
-        while (i<j){
+         *  "A man, a plan, a canal: Panama"
+         *   i                            j
+         * remove non-alphanumeric chars
+         *
+         *  "amanaplanacanalpanama"
+         *            ij
+         *
+         *
+         * */
+        while (i < j) {
             char left = s.charAt(i),
                     right = s.charAt(j);
             // remove non-alphanumeric characters
-            if (!Character.isLetterOrDigit(left)){
-                i=i+1;
+            if (!Character.isLetterOrDigit(left)) {
+                i = i + 1;
                 continue;
             }
-            if (!Character.isLetterOrDigit(right)){
+            if (!Character.isLetterOrDigit(right)) {
                 j--;
                 continue;
             }
             //  uppercase -> lowercase
-            if (Character.toLowerCase(left) != Character.toLowerCase(right)){
+            if (Character.toLowerCase(left) != Character.toLowerCase(right)) {
                 return false;
             }
             i++;
@@ -40,42 +42,44 @@ public class Solution {
         return true;
 
     }
-    public void reverseString(char[] s){
-        int i=0,
-                j=s.length-1;
+
+    public void reverseString(char[] s) {
+        int i = 0,
+                j = s.length - 1;
         /*DRY - RUN
-        * {'o','l','l','e','h'}
-        *           ij
-        *
-        * */
-        while (i<j){
+         * {'o','l','l','e','h'}
+         *           ij
+         *
+         * */
+        while (i < j) {
             char temp = s[i];
-            s[i]=s[j];
-            s[j]=temp;
+            s[i] = s[j];
+            s[j] = temp;
             i++;
             j--;
         }
     }
-    public int[] sortedSquares(int[] nums){
-        int i=0,
-                j=nums.length-1;
-        int k = nums.length-1;
+
+    public int[] sortedSquares(int[] nums) {
+        int i = 0,
+                j = nums.length - 1;
+        int k = nums.length - 1;
 
         int[] res = new int[nums.length];
         /*
-        * [-4,-1,0,3,10]
-        *      i j
-        *  k
-        *  0 1 9  16  100
-        *
-        * */
-        while (i<j){
-            if (Math.abs(nums[i]) > Math.abs(nums[j])){
-                res[k]=nums[i]*nums[i];
+         * [-4,-1,0,3,10]
+         *      i j
+         *  k
+         *  0 1 9  16  100
+         *
+         * */
+        while (i < j) {
+            if (Math.abs(nums[i]) > Math.abs(nums[j])) {
+                res[k] = nums[i] * nums[i];
                 i++;
 
-            }else {
-                res[k]=nums[j]*nums[j];
+            } else {
+                res[k] = nums[j] * nums[j];
                 j--;
 
             }
@@ -84,45 +88,48 @@ public class Solution {
         }
         return res;
     }
-    public boolean validPalindrome(String s){
-        int i =0,
-                j=s.length()-1;
+
+    public boolean validPalindrome(String s) {
+        int i = 0,
+                j = s.length() - 1;
         /*
-        * "aba"
-        *   j
-        *   i no delete
-        *
-        * "abca"
-        *   ij -> mismatch need to delete delete c = aba
-        * "aba"
-        *   j
-        *   i -> valid palindrome
-        *
-        * */
-        while (i<=j){
-            if (s.charAt(i) == s.charAt(j)){
+         * "aba"
+         *   j
+         *   i no delete
+         *
+         * "abca"
+         *   ij -> mismatch need to delete delete c = aba
+         * "aba"
+         *   j
+         *   i -> valid palindrome
+         *
+         * */
+        while (i <= j) {
+            if (s.charAt(i) == s.charAt(j)) {
                 i++;
                 j--;
-            }else {
-                return isPalindrome(s,i+1,j) || isPalindrome(s,i,j-1);
+            } else {
+                return isPalindrome(s, i + 1, j) || isPalindrome(s, i, j - 1);
             }
         }
         return true;
     }
-    public static boolean isPalindrome(String s , int i , int j){
-       while (i<=j){
-           if (s.charAt(i) != s.charAt(j)){
-               return false;
-           }
-           i++;
-           j--;
-       }
+
+    public static boolean isPalindrome(String s, int i, int j) {
+        while (i <= j) {
+            if (s.charAt(i) != s.charAt(j)) {
+                return false;
+            }
+            i++;
+            j--;
+        }
 
         return true;
     }
-    public boolean validWordAbbreviation(String word, String abbr){
-        int i=0,
-                j=0;
+
+    public boolean validWordAbbreviation(String word, String abbr) {
+        int i = 0,
+                j = 0;
 
         /*
         * word = "implementation"
@@ -149,27 +156,27 @@ Agla character '2' bhi digit hai → curr = 1*10 + (2) = 12
 Agla character 'n' digit nahi hai → loop ruk gaya, curr = 12 final.
 Isi ko kehte hain parse integer from string.
         * */
-        while (i<word.length() && j<abbr.length()){
+        while (i < word.length() && j < abbr.length()) {
             char w_c = word.charAt(i);
             char a_c = abbr.charAt(j);
 
-            if (Character.isDigit(a_c)){
+            if (Character.isDigit(a_c)) {
                 //case.1 0
-                if (a_c == '0'){
+                if (a_c == '0') {
                     return false;
                 }
                 //case.3 handle number
-                int curr=0;
-                while (j<abbr.length() && Character.isDigit(abbr.charAt(j))){
-                    curr = curr*10+(abbr.charAt(j)-'0');
+                int curr = 0;
+                while (j < abbr.length() && Character.isDigit(abbr.charAt(j))) {
+                    curr = curr * 10 + (abbr.charAt(j) - '0');
                     j++;
                 }
                 //add skip number in word
-                i+=curr;
+                i += curr;
 
-            }else {
+            } else {
                 //case.2 mismatch
-                if (a_c != w_c){
+                if (a_c != w_c) {
                     return false;
                 }
                 i++;
@@ -177,10 +184,132 @@ Isi ko kehte hain parse integer from string.
             }
         }
         //return same length of both string after checking
-       return i==word.length() && j==abbr.length();
+        return i == word.length() && j == abbr.length();
+    }
+
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
+
+        /*
+          [1,2,2,3,5,6] = m : 3 , i = 3-1 = 2
+        i
+        k     3>6 = J , 3>5 J, 2>3 i, 2>2 i, 1>2 i
+          [2,5,6] = n : 3 , j = 3-1 = 2
+           j
+
+          k=m+n-1 , k = 3+3-1 , 5
+
+        */
+
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] > nums2[j]) {
+                nums1[k] = nums1[i];
+                i--;
+            } else {
+                nums1[k] = nums2[j];
+                j--;
+            }
+            k--;
+        }
+
+        //remaining element in nums2
+        while (j >= 0) {
+            nums1[k] = nums2[j];
+            j--;
+            k--;
+        }
+    }
+    public int countPairs(List<Integer> nums, int target){
+        int count=0;
+        int i =0,
+                j = nums.size()-1;
+
+        /*   0 1 2 3 4
+           [-1,1,2,3,1]
+               i j   --> j-i = 4-1 = 3
+
+        */
+        while (i<j){
+            if (nums.get(i) + nums.get(j) ==target){
+                count+=(j-i);
+                i++;
+            }
+            j--;
+        }
+        return count;
+    }
+
+    public int[] twoSum(int[] nums, int target){
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for(int i=0; i<nums.length; i++){
+            int x = target-nums[i];
+            if(map.containsKey(x)){
+                return new int[] {map.get(x), i};
+            }
+            map.put(nums[i],i);
+        }
+        return new int[]{-1,-1};
+    }
+    public int[] twoSum2(int[] nums, int target){
+        int i =0,
+                j = nums.length-1;
+
+        while(i<j){
+            int sum = nums[i] + nums[j];
+            if(sum<target)
+                i++;
+            else if(sum>target)
+                j--;
+            else
+                return new int[] {i+1,j+1};
+
+        }
+        return new int[] {-1,-1};
+    }
+    public List<List<Integer>> threeSum(int[] nums){
+        //create list of lists for res
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(nums);
+        //rest i & skip duplicate
+        for (int i=0; i<nums.length; i++){
+            if (i >0 && nums[i] == nums[i-1]){
+                continue;
+            }
+            // setup two pinters
+            int j = i+1;
+            int k = nums.length-1;
+
+            while (j<k){
+                int sum = nums[i] + nums[j] + nums[k];
+
+                if (sum<0)
+                    j++;
+                else if (sum>0) {
+                    k--;
+                }
+                else {
+                    // add triple in res asList
+                    res.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    j++;
+                    // skip dupilcate triplet
+                    while (nums[j] == nums[j-1] && j<k){
+                        j++;
+                    }
+
+                }
+            }
+
+        }
+
+
+
+        // return res
+        return res;
     }
 }
-
 class CheckSolution{
     public static void main(String[] args) {
         Solution solution = new Solution();
@@ -201,10 +330,49 @@ class CheckSolution{
        /* String s = "abahf";
         System.out.println(solution.validPalindrome(s));*/
 
-        String word = "implementationjg";
+       /* String word = "implementationjg";
         String abbr = "i12n";
-        System.out.println(solution.validWordAbbreviation(word,abbr));
+        System.out.println(solution.validWordAbbreviation(word,abbr));*/
 
+/*        int[] nums1 = {1,2,3,0,0,0};
+        int m =3;
+        int[] nums2 = {2,5,6};
+        int n =3;
+        solution.merge(nums1,m,nums2,n);
+
+        for(int x : nums1){
+            System.out.print(x + " ");
+        }*/
+
+     /*   List<Integer> nums = new ArrayList<>();
+        nums.add(-1);
+        nums.add(1);
+        nums.add(2);
+        nums.add(3);
+        nums.add(1);
+
+        int target = 2;
+
+        System.out.println(solution.countPairs(nums,target));*/
+
+     /*   int[] nums = {2,7,9,11,15};
+        int target = 9;
+        int[] twoSum = solution.twoSum(nums, target);
+        for (int n : twoSum){
+            System.out.println(n + " ");
+        }*/
+
+      /*     int[] nums = {2,7,9,11,15};
+        int target = 9;
+        int[] twoSum = solution.twoSum2(nums, target);
+        for (int n : twoSum){
+            System.out.println(n + " ");
+        }*/
+        int[] nums = {-1,0,1,2,-1,-4};
+        List<List<Integer>> threeSum = solution.threeSum(nums);
+        for (List<Integer> triplet : threeSum){
+            System.out.print(triplet + " ");
+        }
 
     }
 }
